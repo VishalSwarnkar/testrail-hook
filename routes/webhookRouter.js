ad5ec43bfe13464823e8e51d19a557fbf11fd921
Vishal Swarnkar <vishalswa@gmail.com>
2017-11-28 18:59:24 +0000

    converted array to string may resolve the issue

--- routes/webhookRouter.js
+++ routes/webhookRouter.js
@@ -9,24 +9,24 @@ webhookRouter.use(bodyParser.json());
 webhookRouter.route('/')
 .post((req, res, next) => {
   // if(res) {
-    response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
-
-    res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-    res.send(JSON.stringify({ "speech": response, "displayText": response
-    //"speech" is the spoken version of the response, "displayText" is the visual version
-    }));
+    // response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
+    //
+    // res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+    // res.send(JSON.stringify({ "speech": response, "displayText": response
+    // //"speech" is the spoken version of the response, "displayText" is the visual version
+    // }));
         // result = req.get("result")
         // parameters = result.get("parameters")
-    // projectId = req.body.result.parameters.projectId;
-    // console.log(`Request body details ${JSON.stringify(req.body)}`);
-    // releases.fetch(projectId).then((regressionSuites)=>{
-    //   res.statusCode = 200
-    //   res.setHeader('Content-Type', 'application/json');
-    //   var regressionList = JSON.parse(regressionSuites).map(a=> a.name)
-    //   // res.json(regressionList);
-    //   res.send(JSON.stringify({"speech": regressionList, "displayText": regressionList.slice(0, 5)}));
-    // }, (err)=>next(err))
-    // .catch((error)=>next(error));
+    projectId = req.body.result.parameters.projectId;
+    console.log(`Request body details ${JSON.stringify(req.body)}`);
+    releases.fetch(projectId).then((regressionSuites)=>{
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'application/json');
+      var regressionList = JSON.parse(regressionSuites).map(a=> a.name)
+      // res.json(regressionList);
+      res.send(JSON.stringify({"speech": regressionList.toString(), "displayText": regressionList.toString()}));
+    }, (err)=>next(err))
+    .catch((error)=>next(error));
 
   // }else{
   //   var error = new Error('Unble to get the response');
